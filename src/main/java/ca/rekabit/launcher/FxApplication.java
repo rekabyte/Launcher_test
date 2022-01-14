dccb3f3cb7542bb68d72a4336cad3d4b70364cde
khalil reghaye <khalilplaybox@outlook.fr>
2022-01-14 17:35:19 -0500

    Login panel partiellement fait (design oui, backend non)

--- src/main/java/ca/rekabit/launcher/FxApplication.java
+++ src/main/java/ca/rekabit/launcher/FxApplication.java
@@ -17,48 +17,6 @@ import javafx.stage.Stage;
 
 public class FxApplication extends Application {
 
-
-    private Parent createContent() {
-        VBox vBox = new VBox();
-        GridPane gp = new GridPane();
-        Label lblTitle = new Label("Support Ticket");
-        Label lblEmail = new Label("Email");
-        Label lblPriority = new Label("Priority");
-        Label lblProblem = new Label("Problem");
-        Label lblDescription = new Label("Description");
-        TextField tfEmail = new TextField();
-        TextField tfProblem = new TextField();
-        TextArea taDescription = new TextArea();
-        ObservableList<String> priorities = FXCollections.observableArrayList("Medium", "High", "Low");
-        ComboBox<String> cbPriority = new ComboBox<>(priorities);
-        Separator sep = new Separator();
-        ButtonBar buttonBar = new ButtonBar();
-        Button saveButton = new Button("Save");
-        Button cancelButton = new Button("Cancel");
-
-        buttonBar.setPadding(new Insets(10.0d));
-        ButtonBar.setButtonData(saveButton, ButtonBar.ButtonData.OK_DONE);
-        ButtonBar.setButtonData(cancelButton, ButtonBar.ButtonData.CANCEL_CLOSE);
-        buttonBar.getButtons().addAll(saveButton, cancelButton);
-
-        vBox.getChildren().addAll(gp, sep, buttonBar);
-
-        gp.setPadding(new Insets(10));
-        gp.setHgap(4);
-        gp.setVgap(8);
-
-        VBox.setVgrow(gp, Priority.ALWAYS);
-
-        gp.add(lblTitle, 1, 1);
-        gp.add(lblEmail, 0, 2);     gp.add(tfEmail,1, 2);
-        gp.add(lblPriority, 0, 3);  gp.add(cbPriority, 1, 3);
-        gp.add(lblProblem, 0, 4);   gp.add(tfProblem, 1, 4);
-        gp.add(lblDescription, 0, 5);gp.add(taDescription, 1, 5);
-
-        return vBox;
-    }
-
-
     @Override
     public void start(Stage stage) throws Exception {
         MainPanel mainPanel = new MainPanel(stage, 999, 700);
--- src/main/java/ca/rekabit/launcher/ui/MainPanel.java
+++ src/main/java/ca/rekabit/launcher/ui/MainPanel.java
@@ -4,18 +4,29 @@ import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIcon;
 import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIconView;
 import javafx.geometry.HPos;
 import javafx.geometry.VPos;
+import javafx.scene.Cursor;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.Separator;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 
 public class MainPanel {
 
     private Stage stage;
-    private GridPane layout, topLayout;
+    private GridPane layout, topLayout, loginPanel, mainPanel, bottomPanel;
     private double WIDTH, HEIGHT;
     private Label NOM, empty1, empty2;
 
@@ -26,6 +37,154 @@ public class MainPanel {
     }
 
     public void createScene() {
+        createTopPanel();
+        createLoginPanel();
+        stage.show();
+    }
+
+    private void createLoginPanel() {
+        loginPanel = new GridPane();
+        mainPanel = new GridPane();
+        bottomPanel = new GridPane();
+        AtomicBoolean connectWithMojang = new AtomicBoolean(false);
+
+        loginPanel.setMaxWidth(400);loginPanel.setMinWidth(400);
+        loginPanel.setMaxHeight(580);loginPanel.setMinHeight(580);
+
+        GridPane.setVgrow(loginPanel, Priority.ALWAYS);
+        GridPane.setHgrow(loginPanel, Priority.ALWAYS);
+        GridPane.setHalignment(loginPanel, HPos.CENTER);
+        GridPane.setValignment(loginPanel, VPos.CENTER);
+
+        RowConstraints bottomConstraints = new RowConstraints();
+        bottomConstraints.setValignment(VPos.BOTTOM);
+        bottomConstraints.setMaxHeight(55);
+        loginPanel.getRowConstraints().addAll(new RowConstraints(), bottomConstraints);
+        loginPanel.add(mainPanel,0,0);
+        loginPanel.add(bottomPanel,0,1);
+
+        GridPane.setVgrow(mainPanel, Priority.ALWAYS);
+        GridPane.setHgrow(mainPanel, Priority.ALWAYS);
+        GridPane.setVgrow(bottomPanel, Priority.ALWAYS);
+        GridPane.setHgrow(bottomPanel, Priority.ALWAYS);
+
+        loginPanel.setStyle("-fx-background-color: #181818");
+        bottomPanel.setStyle("-fx-background-color: #181818;");
+        bottomPanel.setOpacity(0.5);
+        Label noAccount = new Label("Vous n'avez pas encore de compte ?");
+        Label registerHere = new Label("S'inscire ici");
+
+        GridPane.setVgrow(noAccount, Priority.ALWAYS);
+        GridPane.setHgrow(noAccount, Priority.ALWAYS);
+        GridPane.setHalignment(noAccount, HPos.CENTER);
+        GridPane.setValignment(noAccount, VPos.TOP);
+        noAccount.setStyle("-fx-text-fill: #bcc6e7; -fx-font-size: 14px;");
+
+        GridPane.setVgrow(registerHere, Priority.ALWAYS);
+        GridPane.setHgrow(registerHere, Priority.ALWAYS);
+        GridPane.setHalignment(registerHere, HPos.CENTER);
+        GridPane.setValignment(registerHere, VPos.BOTTOM);
+        registerHere.setStyle("-fx-text-fill: #69a7ed; -fx-font-size: 14px;");
+        registerHere.setUnderline(true);
+        registerHere.setTranslateY(-10);
+        registerHere.setOnMouseEntered(e-> this.layout.setCursor(Cursor.HAND));
+        registerHere.setOnMouseExited(e-> this.layout.setCursor(Cursor.DEFAULT));
+        registerHere.setOnMouseClicked(e-> {
+            if(connectWithMojang.get()) openURL("https://www.minecraft.net/fr-fr");
+            else openURL("https://www.google.fr");
+        });
+
+        bottomPanel.getChildren().addAll(noAccount, registerHere);
+        loginPanel.setGridLinesVisible(true);
+        layout.add(loginPanel,0,1);
+
+        Label connectLabel = new Label("SE CONNECTER");
+        GridPane.setHgrow(connectLabel, Priority.ALWAYS);
+        GridPane.setVgrow(connectLabel, Priority.ALWAYS);
+        GridPane.setValignment(connectLabel, VPos.TOP);
+        connectLabel.setTranslateX(27);
+        connectLabel.setTranslateY(37.5);
+        connectLabel.setStyle("-fx-text-fill: #bcc6e7; -fx-font-size: 16px;");
+
+        Separator connectSeparator = new Separator();
+        GridPane.setHgrow(connectSeparator, Priority.ALWAYS);
+        GridPane.setVgrow(connectSeparator, Priority.ALWAYS);
+        GridPane.setValignment(connectSeparator, VPos.TOP);
+        GridPane.setHalignment(connectSeparator, HPos.CENTER);
+        connectSeparator.setTranslateY(70);
+        connectSeparator.setMinWidth(350);connectSeparator.setMaxWidth(350);
+        connectSeparator.setStyle("-fx-background-color: white;");
+        connectSeparator.setOpacity(0.5);
+
+        Separator usernameSeparator = new Separator();
+        GridPane.setHgrow(usernameSeparator, Priority.ALWAYS);
+        GridPane.setVgrow(usernameSeparator, Priority.ALWAYS);
+        GridPane.setValignment(usernameSeparator, VPos.TOP);
+        GridPane.setHalignment(usernameSeparator, HPos.CENTER);
+        usernameSeparator.setTranslateY(175);
+        usernameSeparator.setMinWidth(325);usernameSeparator.setMaxWidth(325);
+        usernameSeparator.setMaxHeight(1);
+        usernameSeparator.setStyle("-fx-background-color: white;");
+        usernameSeparator.setOpacity(0.5);
+
+        Label usernameLabel = new Label("Nom d'utilisateur");
+        GridPane.setHgrow(usernameLabel, Priority.ALWAYS);
+        GridPane.setVgrow(usernameLabel, Priority.ALWAYS);
+        GridPane.setValignment(usernameLabel, VPos.TOP);
+        GridPane.setHalignment(usernameLabel, HPos.LEFT);
+        usernameLabel.setStyle("-fx-text-fill: #95bad3; -fx-font-size: 14px;");
+        usernameLabel.setTranslateY(110);
+        usernameLabel.setTranslateX(37);
+
+        TextField usernameField = new TextField();
+        GridPane.setHgrow(usernameField, Priority.ALWAYS);
+        GridPane.setVgrow(usernameField, Priority.ALWAYS);
+        GridPane.setValignment(usernameField, VPos.TOP);
+        GridPane.setHalignment(usernameField, HPos.LEFT);
+        usernameField.setMaxWidth(325);
+        usernameField.setMaxHeight(40);
+        usernameField.setTranslateX(37.5);
+        usernameField.setTranslateY(140);
+        usernameField.setStyle("-fx-text-fill: #e5e5e5; -fx-font-size: 16px; -fx-background-color: #1e1e1e");
+
+        Separator passwordSeparator = new Separator();
+        GridPane.setHgrow(passwordSeparator, Priority.ALWAYS);
+        GridPane.setVgrow(passwordSeparator, Priority.ALWAYS);
+        GridPane.setValignment(passwordSeparator, VPos.TOP);
+        GridPane.setHalignment(passwordSeparator, HPos.CENTER);
+        passwordSeparator.setTranslateY(275);
+        passwordSeparator.setMinWidth(325);passwordSeparator.setMaxWidth(325);
+        passwordSeparator.setMaxHeight(1);
+        passwordSeparator.setStyle("-fx-background-color: white;");
+        passwordSeparator.setOpacity(0.5);
+
+        Label passwordLabel = new Label("Mot de passe");
+        GridPane.setHgrow(passwordLabel, Priority.ALWAYS);
+        GridPane.setVgrow(passwordLabel, Priority.ALWAYS);
+        GridPane.setValignment(passwordLabel, VPos.TOP);
+        GridPane.setHalignment(passwordLabel, HPos.LEFT);
+        passwordLabel.setStyle("-fx-text-fill: #95bad3; -fx-font-size: 14px;");
+        passwordLabel.setTranslateY(200);
+        passwordLabel.setTranslateX(37);
+
+        PasswordField passwordField = new PasswordField();
+        GridPane.setHgrow(passwordField, Priority.ALWAYS);
+        GridPane.setVgrow(passwordField, Priority.ALWAYS);
+        GridPane.setValignment(passwordField, VPos.TOP);
+        GridPane.setHalignment(passwordField, HPos.LEFT);
+        passwordField.setMaxWidth(325);
+        passwordField.setMaxHeight(40);
+        passwordField.setTranslateX(37.5);
+        passwordField.setTranslateY(230);
+        passwordField.setStyle("-fx-text-fill: #e5e5e5; -fx-font-size: 16px; -fx-background-color: #1e1e1e");
+
+
+        mainPanel.getChildren().addAll(connectLabel, connectSeparator, usernameLabel, usernameField, usernameSeparator
+        ,passwordField, passwordLabel, passwordSeparator);
+
+    }
+
+    private void createTopPanel() {
         RowConstraints topControlsConstraints = new RowConstraints();
         ColumnConstraints col1 = new ColumnConstraints();
         ColumnConstraints col2 = new ColumnConstraints();
@@ -78,8 +237,8 @@ public class MainPanel {
 
         layout.getRowConstraints().addAll(topControlsConstraints, new RowConstraints());
 
-        //layout.setStyle("-fx-background-color: #061938;");
-        layout.setStyle(setResponsiveBackground("https://www.umontreal.ca/public/www/images/galerie/3e-niveau/im_campus-mtl.jpg"));
+        layout.setStyle("-fx-background-color: #061938;");
+        //layout.setStyle(setResponsiveBackground("https://www.umontreal.ca/public/www/images/galerie/3e-niveau/im_campus-mtl.jpg"));
         layout.add(topLayout, 0,0);
 
         topLayout.getColumnConstraints().addAll(col1,col2, col3, col4, col5);
@@ -100,8 +259,14 @@ public class MainPanel {
         GridPane.setHalignment(this.NOM, HPos.CENTER);
         GridPane.setHalignment(hide, HPos.RIGHT);
         GridPane.setHalignment(close, HPos.CENTER);
+    }
 
-        stage.show();
+    private void openURL(String url) {
+        try {
+            Desktop.getDesktop().browse(new URI(url));
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }
     }
 
     private String setResponsiveBackground(String url) {
@@ -109,5 +274,7 @@ public class MainPanel {
                 "-fx-background-attachment: fixed; -fx-background-origin: center; -fx-background-position: center;" +
                 "-fx-background-size: cover;";
     }
+
+
 
 }
